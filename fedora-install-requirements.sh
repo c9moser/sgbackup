540085240684a7bc15f9196835d3b86cbc873693
Christian Moser <c9moser@gmail.com>
2025-01-04 16:03:30 +0100

    fedora-install-requirements.sh

--- fedora-install-requirements.sh
+++ fedora-install-requirements.sh
@@ -2,4 +2,4 @@
 # vim: ts=4 sts=4 sw=4 syn=sh smartindent expandtab
 #
 
-sudo dnf install -y gtk4.0 python python-gobject rapidfuzz
+sudo dnf install -y gtk4 python python-gobject python3-rapidfuzz

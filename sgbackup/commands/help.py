a9db15a3a59f3b9800c3341fc3023985ea41e79c
Christian Moser <christian@cmoser.eu>
2025-03-01 20:03:44 +0100

    2025.03.01 20:03:44 (desktop)

--- sgbackup/commands/help.py
+++ sgbackup/commands/help.py
@@ -1,6 +1,6 @@
 ###############################################################################
 # sgbackup - The SaveGame Backup tool                                         #
-#    Copyright (C) 2024  Christian Moser                                      #
+#    Copyright (C) 2024,2025  Christian Moser                                 #
 #                                                                             #
 #    This program is free software: you can redistribute it and/or modify     #
 #    it under the terms of the GNU General Public License as published by     #
